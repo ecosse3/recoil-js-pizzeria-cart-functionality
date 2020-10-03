d4afa3be3276118ca516907c2975eafc2430c267
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2020-10-03 23:52:44 +0200

    feat(home): implement Card component in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
+
 import Hero from '../../components/Hero';
+import Card from '../../components/Card';
+
+import Image1 from '../../assets/images/card1.png';
 
 import { Heading } from '../../utils/typography';
 import { useIsMobile } from '../../utils/theme';
@@ -17,7 +21,18 @@ const Home = () => {
         <Grid>
           <Row center="xs">
             <Col>
-              <Heading mt={60}>Our menu</Heading>
+              <Heading mb={60}>Our menu</Heading>
+            </Col>
+          </Row>
+          <Row center="xs">
+            <Col>
+              <Card image={Image1} title="Pepperoni Pizza" description="Premium pepperoni and cheese is made with real mozzarella on original hand-tossed crust." price="21,99 zł" priceOld="26,99 zł" />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
             </Col>
           </Row>
         </Grid>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,5 +5,6 @@ export const SliderSection = styled.div`
   position: relative;
   display: flex;
   background: linear-gradient(180deg, rgba(255, 255, 255, 0.5) 0%, rgba(197, 197, 197, 0.5) 100%), url(${FoodPattern});
-  height: calc(100vh - 380px);
+  min-height: calc(100vh - 380px);
+  padding: 40px 0 60px 0;
 `;
