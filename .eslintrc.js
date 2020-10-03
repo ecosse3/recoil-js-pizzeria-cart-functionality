f80974800dfae584abf095adab74dc79d239a156
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2020-10-03 20:23:58 +0200

    styles(font): add Montserrat header font

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,9 +41,10 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     "react/jsx-no-target-blank": 0,
     "react/jsx-one-expression-per-line": 0,
-    "react/jsx-props-no-spreading": "warn",
+    "react/jsx-props-no-spreading": 0,
     "react/prop-types": "warn",
     "react/require-default-props": 0,
     "react/forbid-prop-types": 0,
+    "react/destructuring-assignment": 0,
   },
 };
