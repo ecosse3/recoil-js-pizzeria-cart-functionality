ce5d455321b53690121975f0b38958ef1bb5fbdb
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2020-10-04 22:43:22 +0200

    feat(cart): add material-ui table to Cart page

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.5) 0%, rgba(197, 197, 197, 0.5) 100%);
+  min-height: calc(100vh - 380px);
+  padding: 40px 0 60px 0;
+`;
