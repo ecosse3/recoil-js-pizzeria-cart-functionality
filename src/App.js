c59a1c3c24e5c80eedc9a3d2831fa1cf74bb31a3
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2020-10-04 19:57:44 +0200

    feat(home): provide theme globally

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { theme } from './utils/theme';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/cart"><Cart /></Route>
-      </Switch>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/cart"><Cart /></Route>
+        </Switch>
+        <Footer />
+      </ThemeProvider>
     </Router>
   );
 }
