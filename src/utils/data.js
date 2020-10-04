c76654b2ee794da501b460606dae74ed0c58859c
Kurpiewski, Lukasz <luk.kurpiewski@gmail.com>
2020-10-04 18:37:39 +0200

    feat(data): add mock-up data

---
+++ src/utils/data.js
@@ -0,0 +1,44 @@
+import Card1 from '../assets/images/card1.png';
+import Card2 from '../assets/images/card2.png';
+import Card3 from '../assets/images/card3.png';
+import Card4 from '../assets/images/card4.png';
+
+const data = [
+  {
+    id: 1,
+    image: Card1,
+    title: 'Pepperoni Pizza',
+    description: 'Premium pepperoni and cheese is made with real mozzarella on original hand-tossed crust.',
+    price: '23,90 zł',
+    priceOld: '29,90 zł',
+    icons: ['hot', 'wheat']
+  },
+  {
+    id: 2,
+    image: Card2,
+    title: 'Tortellini',
+    description: 'Pasta stuffed with beef and pork topped with your choice of cream sauce with bacon, bolognese or marinara sauce.',
+    price: '17,90 zł',
+    priceOld: '22,90 zł',
+    icons: ['vege', 'hot', 'wheat']
+  },
+  {
+    id: 3,
+    image: Card3,
+    title: 'Strawberry Cake',
+    description: 'Three layer strawberry dessert is not only beautiful looking, but also delicious! Perfect dessert for spring and summer.',
+    price: '13,90 zł',
+    priceOld: '18,90 zł',
+    icons: ['wheat']
+  },
+  {
+    id: 4,
+    image: Card4,
+    title: 'Chefs Pizza',
+    description: 'Premium pepperoni and cheese is made with real mozzarella on original hand-tossed crust.',
+    price: '27,90 zł',
+    icons: ['wheat']
+  }
+];
+
+export default data;
